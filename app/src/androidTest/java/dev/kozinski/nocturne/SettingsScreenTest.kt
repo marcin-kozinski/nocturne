7324c47fe31675c60038bd464efb2ef45702ab88
Marcin Koziński <marcin.m.kozinski+gh@gmail.com>
2025-12-07 09:07:09 +0100

    Add a settings toggle

--- app/src/androidTest/java/dev/kozinski/nocturne/SettingsScreenTest.kt
+++ app/src/androidTest/java/dev/kozinski/nocturne/SettingsScreenTest.kt
@@ -1,20 +1,51 @@
 package dev.kozinski.nocturne
 
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.isToggleable
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onSiblings
+import androidx.compose.ui.test.performClick
 import dev.kozinski.nocturne.ui.theme.NocturneTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class SettingsScreenTest {
     @get:Rule val compose = createComposeRule()
 
-    @Test
-    fun isDisplayed() {
-        val name = "Android"
-        compose.setContent { NocturneTheme { SettingsScreen(name) } }
+    @Before
+    fun before() {
+        compose.setContent { NocturneTheme { SettingsScreen(SettingsViewModel()) } }
+    }
 
-        compose.onNodeWithText("Hello $name!").assertIsDisplayed()
+    @Test
+    fun `calendar enabled label is displayed`() {
+        compose.onCalendarEnabledLabel().assertIsDisplayed()
+    }
+
+    @Test
+    fun `calendar enabled toggle is displayed`() {
+        compose.onCalendarEnabledToggle().assertIsDisplayed()
+    }
+
+    @Test
+    fun `calendar enabled toggle turns on when clicked`() {
+        compose.onCalendarEnabledToggle().assertIsOff().performClick().assertIsOn()
+    }
+
+    private fun SemanticsNodeInteractionsProvider.onCalendarEnabledLabel():
+        SemanticsNodeInteraction {
+        return onNodeWithText("Enable Calendar")
+    }
+
+    private fun SemanticsNodeInteractionsProvider.onCalendarEnabledToggle():
+        SemanticsNodeInteraction {
+        return onCalendarEnabledLabel().onSiblings().filterToOne(isToggleable())
     }
 }
