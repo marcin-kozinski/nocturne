a918cd9b383f8c215a9ef2eecac1949469543be5
Marcin Koziński <marcin.m.kozinski+gh@gmail.com>
2026-04-27 09:08:15 +0200

    Migrate SettingsScreenTest to v2 createComposeRule

--- app/src/androidTest/java/dev/kozinski/nocturne/SettingsScreenTest.kt
+++ app/src/androidTest/java/dev/kozinski/nocturne/SettingsScreenTest.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.test.assertIsOff
 import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.filterToOne
 import androidx.compose.ui.test.isToggleable
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onSiblings
 import androidx.compose.ui.test.performClick
